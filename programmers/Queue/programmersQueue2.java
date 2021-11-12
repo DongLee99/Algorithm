02ec99648f4751db999a83a55ee2e3c18cd49c78
DongLee99 <jhnj741@naver.com>
2021-11-12 11:54:12 +0900

    programmers : 큐

---
+++ programmers/Queue/programmersQueue2.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int priority : priorities) {
+            queue.add(priority);
+        }
+        int result = 0;
+        while(true) {
+            if (location == 0) {
+                Integer poll = queue.poll();
+                int count1 = (int) queue.stream()
+                        .filter(q -> q > poll)
+                        .count();
+                if (count1 != 0) {
+                    queue.remove(0);
+                    location = location + queue.size();
+                    queue.add(poll);
+                } else {
+                    result++;
+                    return result;
+                }
+            } else {
+                Integer poll = queue.poll();
+                int count1 = (int) queue.stream()
+                        .filter(q -> q > poll)
+                        .count();
+                if (count1 != 0) {
+                    queue.remove(0);
+                    queue.add(poll);
+                    location--;
+                } else {
+                    queue.remove(0);
+                    result++;
+                    location--;
+                    continue;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] priorities = {1, 1, 1, 1, 1, 1};
+        int location = 3;
+        Solution solution = new Solution();
+        System.out.println(solution.solution(priorities, location));
+    }
+
+}
