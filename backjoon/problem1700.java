8ff1763883b150212ebf2079cecf739ef65a1d0a
DongLee99 <jhnj741@naver.com>
2022-01-08 16:04:30 +0900

    Backjoon : 1700 실패

---
+++ backjoon/problem1700.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class problem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        int count = 0;
+        int number = 0;
+        int max = 0;
+        int position = 0;
+        boolean result = false;
+
+        ArrayList<Integer> tmp = new ArrayList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < k; i++) {
+            arrayList.add(scanner.nextInt());
+        }
+
+        for (int i = 0; i < k; i++) {
+            if (tmp.size() < x) {
+                if (tmp.contains(arrayList.get(i))) {
+                    continue;
+                } else {
+                    tmp.add(arrayList.get(i));
+                }
+            } else {
+                for (int j = 0; j < x; j++) {
+                    for (int z = i; z < k; z++) {
+                        if (tmp.get(j) == arrayList.get(z)) {
+                            if (z > max) {
+                                max = z;
+                                position = j;
+                                result = true;
+                            }
+                        }
+                    }
+                    if (result == false) {
+                        position = j;
+                        max = 0;
+                        continue;
+                    }
+                    result = false;
+                }
+                if ( max != 0) {
+                    System.out.println(arrayList.get(i));
+                    tmp.remove(position);
+                    tmp.add(arrayList.get(i));
+                    count++;
+                } else {
+                    tmp.remove(position);
+                    tmp.add(arrayList.get(i));
+                }
+                max = 0;
+                position = 0;
+            }
+        }
+        System.out.println(count);
+    }
+}
