6cc2c1d5a11853e9bd03b8729df4b812d6781c02
donglee99 <jhnj741@naver.com>
2023-05-12 16:00:18 +0900

    baekjoon : problem15685.java

--- baekjoon/problem15685.java
+++ baekjoon/problem15685.java
@@ -119,3 +119,28 @@ class main {
         System.out.println(count);
     }
 }
+class Stack {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public Object pop() {
+        if (size = 0)
+            throw new EmptyStackException();
+        return elements[--size];
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size)
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+    }
+}
