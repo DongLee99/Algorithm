a50ac36c421cafe66716b935e459fc40d6737169
donglee99 <jhnj741@naver.com>
2023-05-25 15:23:21 +0900

    baekjoon : 1965 dp

---
+++ baekjoon/problem1965.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int N;
+    public static int[] arr;
+    public static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[N + 1];
+        arr[0] = 0;
+        dp = new int[N + 1];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            answer = Math.max(answer, dp[i]);
+        }
+        System.out.println(answer - 1);
+    }
+}
