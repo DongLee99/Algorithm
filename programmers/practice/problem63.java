dd2cf542f5f0b64b6229c0b30b92e84eb53e03d5
donglee99 <jhnj741@naver.com>
2023-05-26 15:35:44 +0900

    programmers : 와! 틀림!

--- programmers/practice/problem63.java
+++ programmers/practice/problem63.java
@@ -57,3 +57,16 @@ class Solution {
         return answer;
     }
 }
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int count = 0;
+        int index = 0;
+        int[] answer = new int [(int) (right - left + 1)];
+        for (long i = left; i < right + 1; i++) {
+            answer[index] = (int) Math.max(i / n, i % n) + 1;
+            index++;
+        }
+        return answer;
+    }
+}
