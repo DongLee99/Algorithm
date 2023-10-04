e3c886c2331062e203343b6487c5fa8d5bbe8195
donglee99 <jhnj741@naver.com>
2023-10-04 13:23:49 +0900

    baekjoon : 10815 해시

---
+++ baekjoon/problem10815.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static int M;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        Set<Integer> s = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            s.add(sc.nextInt());
+        }
+        M = sc.nextInt();
+        for (int i = 0; i < M; i++) {
+            int tmp = sc.nextInt();
+            if (s.contains(tmp)) {
+                System.out.print(1 + " ");
+            } else {
+                System.out.print(0 + " ");
+            }
+        }
+    }
+}
